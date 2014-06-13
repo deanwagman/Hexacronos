f708bfd4f08df638cea0c4149acaeae5d7035e58
Dean.Wagman <deanwagman@gmail.com>
2014-06-13 00:17:37 -0400

    added tic function

---
+++ scripts/hexacronos.js
@@ -0,0 +1,29 @@
+/* hexacronos.js */
+
+function tic(){
+	var time = new Date(), 
+
+		validate, hours, minutes, seconds, hexcode;
+
+	/* Prevents Date methods from returning single digits */
+	validate = function(num){
+		if(num > 9){
+			return "" + num;
+		} else {
+			return "0" + num;
+		}
+	};
+
+	hours = validate(time.getHours());
+	minutes = validate(time.getMinutes());
+	seconds = validate(time.getSeconds());
+
+	hexcode = "#" + hours + minutes + seconds;
+	body.css('background', hexcode);
+	p.text(hours + " " + minutes + " " + seconds);
+}
+
+var body = $('body'),
+	p = $('p');
+tic();
+setInterval(function(){tic();},1000);
