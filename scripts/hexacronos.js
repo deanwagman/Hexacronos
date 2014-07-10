3d74a2d6c55c51e2d3971b887937c0c3f0131bb2
Dean.Wagman <deanwagman@gmail.com>
2014-07-10 00:09:10 -0400

    Fix syntax for optimization

--- scripts/hexacronos.js
+++ scripts/hexacronos.js
@@ -1,31 +1,35 @@
 /* hexacronos.js */
 
 var body = $('body'),
-	p = $('p');
+    p = $('p');
 
 
-function tic(){
-	var time = new Date(), 
+function tic() {
+    var time = new Date(),
+        validate,
+        hours,
+        minutes,
+        seconds,
+        hexcode;
 
-		validate, hours, minutes, seconds, hexcode;
+    /* Prevents Date methods from returning single digits */
+    validate = function (num) {
+        if (num > 9) {
+            return num.toString();
+        }
+        return "0" + num;
+    };
 
-	/* Prevents Date methods from returning single digits */
-	validate = function(num){
-		if(num > 9){
-			return "" + num;
-		} else {
-			return "0" + num;
-		}
-	};
+    hours = validate(time.getHours());
+    minutes = validate(time.getMinutes());
+    seconds = validate(time.getSeconds());
 
-	hours = validate(time.getHours());
-	minutes = validate(time.getMinutes());
-	seconds = validate(time.getSeconds());
-
-	hexcode = "#" + hours + minutes + seconds;
-	body.css('background', hexcode);
-	p.text(hours + " " + minutes + " " + seconds);
+    hexcode = "#" + hours + minutes + seconds;
+    body.css('background', hexcode);
+    p.text(hours + " " + minutes + " " + seconds);
 }
 
 tic();
-setInterval(function(){tic();},1000);
+setInterval(function () {
+    tic();
+}, 1000);
